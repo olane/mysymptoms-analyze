3a8bcd8545ca0f60266c2acd002ab3c9814da078
Oli Lane <ojgl2@cam.ac.uk>
2018-04-28 14:51:26 +0100

    Write some python to pull out a dictionary of names to all matching ids in the db

---
+++ deduplicate.py
@@ -0,0 +1,25 @@
+import sqlite3
+import itertools
+
+def getNamesToIds(connection):
+    c = connection.cursor()
+    c.execute('''   SELECT 
+                        name,
+                        group_concat(uuid)
+                    FROM ingested
+                        GROUP BY name
+                ''')
+    allRows = c.fetchall()
+
+    nameFunc = lambda x: x[0]
+    groupedByName = itertools.groupby(sorted(allRows, key=nameFunc), nameFunc)
+
+    idsToNames = {key: [thing[1] for thing in group] for key, group in groupedByName}
+    return idsToNames
+
+conn = sqlite3.connect('mySymptoms_deduplicated.sqlite')
+namesToIds = getNamesToIds(conn)
+for name, ids in namesToIds.iteritems():
+    print name
+    print ', '.join(ids)
+conn.close()
